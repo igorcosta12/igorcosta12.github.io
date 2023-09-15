b5ae5ff097671750266d28a2e9e1edee21a19b65
Igor Costa <89418144+igorcosta12@users.noreply.github.com>
2023-09-15 10:36:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function () {
-    const changeTextButton = document.getElementById("changeTextButton");
-    const displayText = document.getElementById("displayText");
 
-    changeTextButton.addEventListener("click", function () {
-        displayText.textContent = "Texto alterado!";
-    });
-});
